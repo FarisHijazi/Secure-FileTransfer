100993dde94bc8c805530084b143c2fa92cd769f
Faris Hijazi <theefaris@gmail.com>
2019-12-25 16:30:43 +0300

    client/server frontends now using authentication

--- scripts/client.py
+++ scripts/client.py
@@ -5,6 +5,8 @@ import inspect
 import os
 import sys
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))  # move path to file dir, to access files 
+ 
 DESCRIPTION = ("COE451 ProgAssignment1: SFTP client-server."
                "\nClient side" +
                "\nFaris Hijazi s201578750 25-09-19." +
@@ -24,16 +26,29 @@ def main():
     print(DESCRIPTION + "\n")
 
     args = None
-
+    authedOnce = False
     i = 0
     while True:
         i += 1
         args = get_user_commands(parser, args)
 
+        if authedOnce:
+            args['auth'] = False
+
         if args['auth']:
             print('Client: authenticating server...')
             args['key'] = authenticate(args)
-
+            if args['key'] == False:
+                print('authentication failed, closing connection')
+                exit(1)
+            else:
+                print('successfully authenticated :D')
+                authedOnce = True
+                continue
+        
+        args['auth'] = False
+        # import time
+        # time.sleep(5)
         resp = exec_function(args)
 
 
--- scripts/server.py
+++ scripts/server.py
@@ -5,7 +5,10 @@ import socket
 
 from utils import recv_msg
 from server_backend import get_arg_parser, recv_next_command
+from authentication import authenticate
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))  # move path to file dir, to access files 
+ 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
@@ -20,11 +23,11 @@ if __name__ == "__main__":
     parser = get_arg_parser()
     args = vars(parser.parse_args())
 
-    from authentication import authenticate
 
     i = 0
     while True:
         i += 1
+        sess = {}
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((args['host'], args['port']))
             s.listen(0)
@@ -33,18 +36,29 @@ if __name__ == "__main__":
             with conn:
                 print('Connected by', addr)
                 client_args = recv_next_command(conn)
+                
                 if client_args['auth']:
-                    authenticate(client_args, is_client=False, conn=conn)
+                    args['key'] = authenticate(client_args, is_client=False, conn=conn)
+                    if args['key'] == False:
+                        print('authentication failed, closing connection')
+                        exit(1)
+                    else:
+                        # sess['key'] = args['key']
+                        print('successfully authenticated :D')
+                        continue
 
-                try:
+                # try:
+                # client_args = recv_next_command(conn)
                 result = client_args['function'](conn, client_args)
 
-                    final_client_resp = recv_msg(conn)
-                    if final_client_resp in [r'200']:
-                        print("Transaction completed successfully")
-                except Exception as e:
-                    print("Error:", e)
-                    continue
+                final_client_resp = recv_msg(conn)
+                if final_client_resp in [r'200']:
+                    print("Transaction completed successfully")
+                else:
+                    print("WARNING: transaction didn't complete successfully: client didn't send TRANSACTION_COMPLETED (200)")
+                # except Exception as e:
+                #     print("Error:", e)
+                #     continue
 
                 # then based on the request, do the action
                 # the request is basically an argument string, can be parsed by the argparser
